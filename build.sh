d243ea79945994b1331dfcbf99c16dc19efbabae
LeeHemingway <coherence0815@gmail.com>
2021-10-07 19:25:39 +0900

    comment non-minified version

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 rm dist/*
-browserify src/js/main.js --s charttable -o dist/charttable.js
+
+# Uncomment this line to see non-minified version of js file for debugging
+# browserify src/js/main.js --s charttable -o dist/charttable.js
+
 browserify src/js/main.js --s charttable | uglifyjs -c > dist/charttable.min.js
+
 uglifycss src/css/main.css > dist/charttable.min.css
 
